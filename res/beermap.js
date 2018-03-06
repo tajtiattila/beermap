3ab8cdb974d68391e54f186922f317f20ed18128
Attila Tajti <attila.tajti@gmail.com>
2018-03-06 10:33:24 +0100

    ui: remove svg icon remnants

--- res/beermap.js
+++ res/beermap.js
@@ -240,17 +240,8 @@ function startMap(mapData) {
   var infowindow = new google.maps.InfoWindow();
 
   var markers = mapData.pubs.map(function(p) {
-    var color;
-    if (p.closed) {
-      color = "#f00";
-    } else if (p.visited) {
-      color = "#0c0";
-    } else {
-      color = "#44f";
-    }
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(p.lat, p.lng),
-        //icon: svgCircleIcon(color, '#fff', p.label)
         icon: {
           url: p.icon,
           scaledSize: new google.maps.Size(28, 28)
@@ -268,23 +259,5 @@ function startMap(mapData) {
   })
 }
 
-function svgCircleIcon(circleFill, labelColor, label) {
-  var template = [
-      '<?xml version="1.0"?>',
-          '<svg width="26px" height="26px" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">',
-              '<circle stroke="{{fill}}" stroke-width="10" stroke-opacity="10%" fill="{{fill}}" cx="50" cy="50" r="35"/>',
-              '<text x="50" y="50" text-anchor="middle" alignment-baseline="central" font-family="Roboto" font-size="40" fill="{{labelFill}}">{{label}}</text>',
-          '</svg>'
-      ].join('\n');
-  var svg = replaceAll(template, "{{fill}}", circleFill);
-  svg = replaceAll(svg, "{{labelFill}}", labelColor);
-  svg = replaceAll(svg, "{{label}}", label);
-  return {
-    url: 'data:image/svg+xml;charset=UTF-8,' + encodeURIComponent(svg),
-    scaledSize: new google.maps.Size(30, 30)
-  };
-}
 
-function replaceAll(target, search, replacement) {
-  return target.replace(new RegExp(search, 'g'), replacement);
 }
